cf116b670002a8c0e841f0aae152030183defede
jhivago75 <jhivago75@gmail.com>
2018-12-04 09:42:26 -0600

    Removed extra Start-Transcript
    Removed extra Start-Transcript that was resulting in a blank file

--- DumpVar.ps1
+++ DumpVar.ps1
@@ -63,14 +63,6 @@ Write-Output "Logging to $logFile"
 #Set Error Action to Silently Continue
 $ErrorActionPreference = "SilentlyContinue"
 
-# Determine where to do the logging
-$tsenv = New-Object -COMObject Microsoft.SMS.TSEnvironment
-$logPath = $tsenv.Value("_SMSTSLogPath")
-$logFile = "$logPath\$($myInvocation.MyCommand).log"
-
-# Start the logging
-Start-Transcript $logFile
-
 # Write all the variables and their values
 $tsenv.GetVariables() | % { Write-Host "$_ = $($tsenv.Value($_))" }
 
