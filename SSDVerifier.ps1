38da48f6a0e7b3041261782eeb85e00fec5cbec1
jhivago75 <jhivago75@gmail.com>
2020-10-01 10:45:31 -0500

    Add files via upload

---
+++ SSDVerifier.ps1
@@ -0,0 +1,390 @@
+<#
+.Synopsis
+    First Boot device verifier
+.DESCRIPTION
+    Used to verify that SSD is indeed first boot device on a system with two hard drives.
+	If necessary, sets a variable needed to switch installation drives.
+.EXAMPLE
+    ResetTPMOwner.ps1
+.NOTES
+    Created:	 2018-05-02
+    Version:	 1.0
+    Author - Anton Romanyuk
+    Twitter: @admiraltolwyn
+    Blog   : http://www.vacuumbreather.com
+    Disclaimer:
+    This script is provided 'AS IS' with no warranties, confers no rights and 
+    is not supported by the author.
+.LINK
+    http://www.vacuumbreather.com
+.NOTES
+
+#>
+
+# Determine where to do the logging 
+$tsenv = New-Object -COMObject Microsoft.SMS.TSEnvironment 
+$logPath = $tsenv.Value("LogPath")  
+$logFile = "$logPath\$($myInvocation.MyCommand).log"
+$Model = $TSenv.Value("Model")
+$disks = @()
+
+# Create Log folder
+$testPath = Test-Path $logPath
+If (!$testPath)
+{
+    New-Item -ItemType Directory -Path $logPath
+}
+ 
+# Create Logfile
+Write-Output "$ScriptName - Create Logfile" > $logFile
+ 
+Function Logit($TextBlock1){
+	$TimeDate = Get-Date
+	$OutPut = "$ScriptName - $TextBlock1 - $TimeDate"
+	Write-Output $OutPut >> $logFile
+}
+
+# http://ramblingcookiemonster.github.io/Join-Object/
+function Join-Object
+{
+    [CmdletBinding()]
+    Param
+    (
+        [Parameter(Mandatory=$true,
+                   ValueFromPipeLine = $true)]
+        [object[]] $Left,
+
+        # List to join with $Left
+        [Parameter(Mandatory=$true)]
+        [object[]] $Right,
+
+        [Parameter(Mandatory = $true)]
+        [string] $LeftJoinProperty,
+
+        [Parameter(Mandatory = $true)]
+        [string] $RightJoinProperty,
+
+        [object[]]$LeftProperties = '*',
+
+        # Properties from $Right we want in the output.
+        # Like LeftProperties, each can be a plain name, wildcard or hashtable. See the LeftProperties comments.
+        [object[]]$RightProperties = '*',
+
+        [validateset( 'AllInLeft', 'OnlyIfInBoth', 'AllInBoth', 'AllInRight')]
+        [Parameter(Mandatory=$false)]
+        [string]$Type = 'AllInLeft',
+
+        [string]$Prefix,
+        [string]$Suffix
+    )
+    Begin
+    {
+        function AddItemProperties($item, $properties, $hash)
+        {
+            if ($null -eq $item)
+            {
+                return
+            }
+
+            foreach($property in $properties)
+            {
+                $propertyHash = $property -as [hashtable]
+                if($null -ne $propertyHash)
+                {
+                    $hashName = $propertyHash["name"] -as [string]         
+                    $expression = $propertyHash["expression"] -as [scriptblock]
+
+                    $expressionValue = $expression.Invoke($item)[0]
+            
+                    $hash[$hashName] = $expressionValue
+                }
+                else
+                {
+                    foreach($itemProperty in $item.psobject.Properties)
+                    {
+                        if ($itemProperty.Name -like $property)
+                        {
+                            $hash[$itemProperty.Name] = $itemProperty.Value
+                        }
+                    }
+                }
+            }
+        }
+
+        function TranslateProperties
+        {
+            [cmdletbinding()]
+            param(
+                [object[]]$Properties,
+                [psobject]$RealObject,
+                [string]$Side)
+
+            foreach($Prop in $Properties)
+            {
+                $propertyHash = $Prop -as [hashtable]
+                if($null -ne $propertyHash)
+                {
+                    $hashName = $propertyHash["name"] -as [string]         
+                    $expression = $propertyHash["expression"] -as [scriptblock]
+
+                    $ScriptString = $expression.tostring()
+                    if($ScriptString -notmatch 'param\(')
+                    {
+                        Write-Verbose "Property '$HashName'`: Adding param(`$_) to scriptblock '$ScriptString'"
+                        $Expression = [ScriptBlock]::Create("param(`$_)`n $ScriptString")
+                    }
+                
+                    $Output = @{Name =$HashName; Expression = $Expression }
+                    Write-Verbose "Found $Side property hash with name $($Output.Name), expression:`n$($Output.Expression | out-string)"
+                    $Output
+                }
+                else
+                {
+                    foreach($ThisProp in $RealObject.psobject.Properties)
+                    {
+                        if ($ThisProp.Name -like $Prop)
+                        {
+                            Write-Verbose "Found $Side property '$($ThisProp.Name)'"
+                            $ThisProp.Name
+                        }
+                    }
+                }
+            }
+        }
+
+        function WriteJoinObjectOutput($leftItem, $rightItem, $leftProperties, $rightProperties)
+        {
+            $properties = @{}
+
+            AddItemProperties $leftItem $leftProperties $properties
+            AddItemProperties $rightItem $rightProperties $properties
+
+            New-Object psobject -Property $properties
+        }
+
+        #Translate variations on calculated properties.  Doing this once shouldn't affect perf too much.
+        foreach($Prop in @($LeftProperties + $RightProperties))
+        {
+            if($Prop -as [hashtable])
+            {
+                foreach($variation in ('n','label','l'))
+                {
+                    if(-not $Prop.ContainsKey('Name') )
+                    {
+                        if($Prop.ContainsKey($variation) )
+                        {
+                            $Prop.Add('Name',$Prop[$Variation])
+                        }
+                    }
+                }
+                if(-not $Prop.ContainsKey('Name') -or $Prop['Name'] -like $null )
+                {
+                    Throw "Property is missing a name`n. This should be in calculated property format, with a Name and an Expression:`n@{Name='Something';Expression={`$_.Something}}`nAffected property:`n$($Prop | out-string)"
+                }
+
+
+                if(-not $Prop.ContainsKey('Expression') )
+                {
+                    if($Prop.ContainsKey('E') )
+                    {
+                        $Prop.Add('Expression',$Prop['E'])
+                    }
+                }
+            
+                if(-not $Prop.ContainsKey('Expression') -or $Prop['Expression'] -like $null )
+                {
+                    Throw "Property is missing an expression`n. This should be in calculated property format, with a Name and an Expression:`n@{Name='Something';Expression={`$_.Something}}`nAffected property:`n$($Prop | out-string)"
+                }
+            }        
+        }
+
+        $leftHash = @{}
+        $rightHash = @{}
+
+        # Hashtable keys can't be null; we'll use any old object reference as a placeholder if needed.
+        $nullKey = New-Object psobject
+        
+        $bound = $PSBoundParameters.keys -contains "InputObject"
+        if(-not $bound)
+        {
+            [System.Collections.ArrayList]$LeftData = @()
+        }
+    }
+    Process
+    {
+        #We pull all the data for comparison later, no streaming
+        if($bound)
+        {
+            $LeftData = $Left
+        }
+        Else
+        {
+            foreach($Object in $Left)
+            {
+                [void]$LeftData.add($Object)
+            }
+        }
+    }
+    End
+    {
+        foreach ($item in $Right)
+        {
+            $key = $item.$RightJoinProperty
+
+            if ($null -eq $key)
+            {
+                $key = $nullKey
+            }
+
+            $bucket = $rightHash[$key]
+
+            if ($null -eq $bucket)
+            {
+                $bucket = New-Object System.Collections.ArrayList
+                $rightHash.Add($key, $bucket)
+            }
+
+            $null = $bucket.Add($item)
+        }
+
+        foreach ($item in $LeftData)
+        {
+            $key = $item.$LeftJoinProperty
+
+            if ($null -eq $key)
+            {
+                $key = $nullKey
+            }
+
+            $bucket = $leftHash[$key]
+
+            if ($null -eq $bucket)
+            {
+                $bucket = New-Object System.Collections.ArrayList
+                $leftHash.Add($key, $bucket)
+            }
+
+            $null = $bucket.Add($item)
+        }
+
+        $LeftProperties = TranslateProperties -Properties $LeftProperties -Side 'Left' -RealObject $LeftData[0]
+        $RightProperties = TranslateProperties -Properties $RightProperties -Side 'Right' -RealObject $Right[0]
+
+        #I prefer ordered output. Left properties first.
+        [string[]]$AllProps = $LeftProperties
+
+        #Handle prefixes, suffixes, and building AllProps with Name only
+        $RightProperties = foreach($RightProp in $RightProperties)
+        {
+            if(-not ($RightProp -as [Hashtable]))
+            {
+                Write-Verbose "Transforming property $RightProp to $Prefix$RightProp$Suffix"
+                @{
+                    Name="$Prefix$RightProp$Suffix"
+                    Expression=[scriptblock]::create("param(`$_) `$_.'$RightProp'")
+                }
+                $AllProps += "$Prefix$RightProp$Suffix"
+            }
+            else
+            {
+                Write-Verbose "Skipping transformation of calculated property with name $($RightProp.Name), expression:`n$($RightProp.Expression | out-string)"
+                $AllProps += [string]$RightProp["Name"]
+                $RightProp
+            }
+        }
+
+        $AllProps = $AllProps | Select -Unique
+
+        Write-Verbose "Combined set of properties: $($AllProps -join ', ')"
+
+        foreach ( $entry in $leftHash.GetEnumerator() )
+        {
+            $key = $entry.Key
+            $leftBucket = $entry.Value
+
+            $rightBucket = $rightHash[$key]
+
+            if ($null -eq $rightBucket)
+            {
+                if ($Type -eq 'AllInLeft' -or $Type -eq 'AllInBoth')
+                {
+                    foreach ($leftItem in $leftBucket)
+                    {
+                        WriteJoinObjectOutput $leftItem $null $LeftProperties $RightProperties | Select $AllProps
+                    }
+                }
+            }
+            else
+            {
+                foreach ($leftItem in $leftBucket)
+                {
+                    foreach ($rightItem in $rightBucket)
+                    {
+                        WriteJoinObjectOutput $leftItem $rightItem $LeftProperties $RightProperties | Select $AllProps
+                    }
+                }
+            }
+        }
+
+        if ($Type -eq 'AllInRight' -or $Type -eq 'AllInBoth')
+        {
+            foreach ($entry in $rightHash.GetEnumerator())
+            {
+                $key = $entry.Key
+                $rightBucket = $entry.Value
+
+                $leftBucket = $leftHash[$key]
+
+                if ($null -eq $leftBucket)
+                {
+                    foreach ($rightItem in $rightBucket)
+                    {
+                        WriteJoinObjectOutput $null $rightItem $LeftProperties $RightProperties | Select $AllProps
+                    }
+                }
+            }
+        }
+    }
+}
+ 
+# Start Main Code Here
+$ScriptName = $MyInvocation.MyCommand.Name
+
+. Logit "$($myInvocation.MyCommand) - Retrieving physical disks"
+
+. Logit "$($myInvocation.MyCommand) - Model set to $Model"
+
+#Switch ($Model){
+#"HP ProDesk 600 G3 MT"{
+    #Note: we need to make sure that SSD is not the first boot device, so we need to evaluate the disk number as well
+	$disksorder = Get-Disk | Select-Object Number,FriendlyName
+    $physicaldisks = Get-PhysicalDisk | Select-Object FriendlyName,MediaType
+    $disks = Join-Object -Left $disksorder -Right $physicaldisks -LeftJoinProperty FriendlyName -RightJoinProperty FriendlyName
+	
+    . Logit "$($myInvocation.MyCommand) - Processing retrieved physical disks."
+	. Logit "$($myInvocation.MyCommand) - " $disks
+
+	foreach($disk in $disks) {
+	    if($disk.MediaType -like "SSD" -and $disk.Number -gt "0") {
+			. Logit "$($myInvocation.MyCommand) - SSD drive detected and it is not the first boot device."
+            $TSenv.Value("IsNVMe") = "TRUE"
+            $TSenv.Value("IsDataDisk") = "TRUE"
+			Exit 0
+		}
+		if($disk.MediaType -like "SSD" -and $disk.Number -eq "0") {
+			. Logit "$($myInvocation.MyCommand) - SSD drive detected and it is the first boot device. No change required."
+			Exit 0
+        }
+        if($disk.MediaType -notlike "SSD" -and $disk.Number -gt "0") {
+            . Logit "$($myInvocation.MyCommand) - Non-SSD Data drive detected."
+            $TSenv.Value("IsDataDisk") = "TRUE"
+            Exit 0
+        }
+	}
+#}
+#Default
+#   {
+#        . Logit "$($myInvocation.MyCommand) - $Model not supported, exit" 
+#        Exit 0
+#    }
+#}
